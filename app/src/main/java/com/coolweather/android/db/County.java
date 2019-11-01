614b3846b00cce2597192b9c4c6fe145d413067a
Ljx12345678 <1632175154@qq.com;ssh-keygen-t rsa-C 1632175154@qq.com>
2019-11-01 11:11:13 +0800

    加入了数据库和表的配置

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,38 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2019/11/1 0001.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private  String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
