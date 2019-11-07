81b04a4ee125daafb676d011782f4c0b626d5595
Ljx12345678 <1632175154@qq.com;ssh-keygen-t rsa-C 1632175154@qq.com>
2019-11-07 08:29:41 +0800

    天气显示

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2019/11/4 0004.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
